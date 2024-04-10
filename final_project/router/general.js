48caef1f55d180b2cae2cbf1a8cdd6da183eb37b
Eduardo Hernández Villa <ehernandezvilla@gmail.com>
2024-04-09 23:12:19 -0400

    get books / endpoint

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -21,8 +21,7 @@ public_users.post("/register", (req,res) => {
 
 // Get the book list available in the shop
 public_users.get('/',function (req, res) {
-  //Write your code here
-  return res.status(200).json({message: "Hello world!"});
+  res.status(200).send(JSON.stringify(books, null, 4));
 });
 
 // Get book details based on ISBN
