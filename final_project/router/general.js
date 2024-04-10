a08c8bb5d64d1d42edbf40e22f4b9c94b8f5aa82
Eduardo Hernández Villa <ehernandezvilla@gmail.com>
2024-04-10 16:46:45 -0400

    author endpoint

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -24,7 +24,7 @@ public_users.get('/',function (req, res) {
   res.status(200).send(JSON.stringify(books, null, 4));
 });
 
-// Get book details based on ISBN (International Standard Book Number)
+// Get book details based on ISBN 
 public_users.get('/isbn/:isbn',function (req, res) {
   const isbn = req.params.isbn;
   const book = Object.values(books).find(book => book.isbn === isbn);
@@ -35,12 +35,18 @@ public_users.get('/isbn/:isbn',function (req, res) {
     return res.status(404).json({message: "Book not found"});
   }
 
- });
+});
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const authorQuery = req.params.author.toLowerCase();
+  const booksByAuthor = Object.values(books).filter(book => book.author.toLowerCase().includes(authorQuery));
+  
+  if (booksByAuthor.length > 0) {
+    return res.status(200).json(booksByAuthor);
+  } else {
+    return res.status(404).json({ message: "No books found for the given author" });
+  }
 });
 
 // Get all books based on title
