5aeb2a4a08f062d2f02f1ddd3813ebdb8312116c
Eduardo Hernández Villa <ehernandezvilla@gmail.com>
2024-04-10 18:21:48 -0400

    adding env

--- index.js
+++ index.js
@@ -3,12 +3,14 @@ const jwt = require('jsonwebtoken');
 const session = require('express-session')
 const customer_routes = require('./router/auth_users.js').authenticated;
 const genl_routes = require('./router/general.js').general;
+require('dotenv').config();
 
 const app = express();
 
 app.use(express.json());
 
-app.use("/customer",session({secret:"fingerprint_customer",resave: true, saveUninitialized: true}))
+app.use("/customer",session({secret:process.env.SESSION_STORE,resave: true, saveUninitialized: true}))
+// console.log(process.env.SESSION_STORE);
 
 app.use("/customer/auth/*", function auth(req, res, next) {
     console.log(req.session); // Debug: Inspect the session
